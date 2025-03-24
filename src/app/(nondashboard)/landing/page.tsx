63b18c621144c572f63823bc12f629b4722129e4
Mohammed <mohammednaamiweb@gmail.com>
2025-03-24 01:12:47 +0000

    fix landing

---
+++ src/app/(nondashboard)/landing/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroSection from './HeroSection'
+import FeaturesSecion from './FeaturesSecion'
+
+const Landing = () => {
+  return (
+    <div>
+        <HeroSection /> 
+        <FeaturesSecion /> 
+    </div>
+  )
+}
+
+export default Landing
---
+++ src/app/providres.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import StoreProvider from "@/state/redux";
+
+
+export default function Providers({ children }: {children: React.ReactNode}) {
+  return (
+    <StoreProvider>
+      {children}
+    </StoreProvider>
+  );
+}
