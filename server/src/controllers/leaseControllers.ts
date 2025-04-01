f272badb9b82a9578b6bacfc1e9cdbd381a14024
Mohammed <mohammednaamiweb@gmail.com>
2025-04-01 13:24:34 +0000

    Fix bugs

--- server/src/controllers/leaseControllers.ts
+++ server/src/controllers/leaseControllers.ts
@@ -4,32 +4,34 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const getLeases = async (req: Request, res: Response): Promise<void> => {
-    try {
+  try {
+    const leases = await prisma.lease.findMany({
+      include: {
+        tenant: true,
+        property: true,
+      },
+    });
+    res.json(leases);
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Error retrieving leases: ${error.message}` });
+  }
+};
 
-        const leases = await prisma.lease.findMany({
-            include: {
-                tenant: true,
-                property: true
-            }
-        });
-        res.json(leases);
-
-    }
-    catch (err: any) {
-        res.status(500).json({ message: "Error retrieving leases" });
-    }
-}
-
-export const getLeasesPayment = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const { id } = req.params;
-
-        const payments = await prisma.payment.findMany({
-            where: { leaseId: Number(id) },
-        });
-        res.json(payments);
-    }
-    catch (err: any) {
-        res.status(500).json({ message: "Error retrieving lease payment" });
-    }
-}
+export const getLeasePayments = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const payments = await prisma.payment.findMany({
+      where: { leaseId: Number(id) },
+    });
+    res.json(payments);
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Error retrieving lease payments: ${error.message}` });
+  }
+};
--- server/src/routes/leasesRoutes.ts
+++ server/src/routes/leasesRoutes.ts
@@ -1,12 +1,14 @@
-import express from 'express';
-import { getLeases, getLeasesPayment } from '../controllers/leaseControllers';
-import { authMiddleware } from '../middleware/authMiddleware';
+import express from "express";
+import { authMiddleware } from "../middleware/authMiddleware";
+import { getLeasePayments, getLeases } from "../controllers/leaseControllers";
+
 const router = express.Router();
 
-
-router.get('/', authMiddleware(["manager", "tenant"]), getLeases);
-router.get('/:id/payements', authMiddleware(["manager", "tenant"]), getLeasesPayment);
-
+router.get("/", authMiddleware(["manager", "tenant"]), getLeases);
+router.get(
+  "/:id/payments",
+  authMiddleware(["manager", "tenant"]),
+  getLeasePayments
+);
 
 export default router;
-
